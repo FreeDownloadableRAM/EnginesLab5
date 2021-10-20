b9b826cc697521e001182aa8d8aa00f7fb5fe4eb
Hossain Alinaqi <hossain.alinaqi@ontariotechu.net>
2021-10-19 21:40:27 -0400

    Can Now Spawn Spheres and Cubes

--- Assets/Scripts/Command Pattern/InputPlane.cs
+++ Assets/Scripts/Command Pattern/InputPlane.cs
@@ -7,6 +7,11 @@ public class InputPlane : MonoBehaviour
     Camera maincam;
     RaycastHit hitInfo;
     public Transform cubePrefab;
+    public Transform spherePrefab;//sphere
+
+    //switch prefab to use
+    private int prefabPicker;
+
 
     // Start is called before the first frame update
     void Awake()
@@ -17,7 +22,20 @@ public class InputPlane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //Pick to use cube or sphere, cube is 1, sphere is anything else. press 1 on keyboard for cube, 2 for sphere.
+
+        if (Input.GetKeyDown("1"))
+        {
+            //change prefabPicker to 1
+            prefabPicker = 1;
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            //change prefabPicker to 1
+            prefabPicker = 2;
+        }
+        //for cube
+        if (Input.GetMouseButtonDown(0) && prefabPicker == 1)
         {
             Ray ray = maincam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
@@ -25,10 +43,25 @@ public class InputPlane : MonoBehaviour
                 Color c = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f));
                 //CubePlacer.PlaceCube(hitInfo.point, c, cubePrefab);
 
-                ICommand command = new PlaceCubeCommand(hitInfo.point, c, cubePrefab);
+                ICommand command = new PlaceCubeCommand(hitInfo.point, c, cubePrefab);//creates the cube
+                //ICommand command = new PlaceCubeCommand(hitInfo.point, c, spherePrefab);//creates the sphere
                 CommandInvoker.AddCopmmand(command);
             }
         }
-        
+        //for sphere
+        if (Input.GetMouseButtonDown(0) && prefabPicker != 1)
+        {
+            Ray ray = maincam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+            {
+                Color c = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f));
+                //CubePlacer.PlaceCube(hitInfo.point, c, cubePrefab);
+
+                //ICommand command = new PlaceCubeCommand(hitInfo.point, c, cubePrefab);//creates the cube
+                ICommand command = new PlaceCubeCommand(hitInfo.point, c, spherePrefab);//creates the sphere
+                CommandInvoker.AddCopmmand(command);
+            }
+        }
+
     }
 }
